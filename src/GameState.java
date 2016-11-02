9d55140cb784ed00d8044713caa913a38b2a37b5
BenBroering <benbroering1@gmail.com>
2016-11-02 11:01:01 -0400

    Added GameStates

--- src/GameState.java
+++ src/GameState.java
@@ -2,11 +2,14 @@
  * Created by Ben on 10/31/2016.
  */
 public class GameState {
-    // Gamestates decide which frames should show up.
+    // Gamestates decide which gui (JFrame) should show up in PsychGUI.java.
     public static final int LOGINREGISTER = 0;
     public static final int JOINCREATE = 1;
+    public static final int JOIN = 2;
+    public static final int JOINWAIT = 3;
+    public static final int CREATE = 4;
+    public static final int SUGGESTION = 5;
+    public static final int PICKING = 6;
+    public static final int RESULTS = 7;
     //public static final int NAMENEEDED = 0;
-    //public static final int NAMENEEDED = 0;
-    //public static final int NAMENEEDED = 0;
-
 }
